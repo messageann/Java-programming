b7dbd85054407b93d99ce1d1fd8ded79f1ced5c7
хм да ето кисулькен <night.jesus.of.suburbia@yandex.ru>
2020-09-14 04:40:53 +0300

    lesson 1

--- practice_2/src/ex_2/Main.java
+++ practice_2/src/ex_2/Main.java
@@ -2,8 +2,7 @@ package ex_2;
 
 public class Main {
     public static void main (String[] args) {
-        String colour = "green";
-        WowBall b = new WowBall(colour);
+        WowBall b = new WowBall("green");
         System.out.println(b.toString());
     }
 }
