ff3716f51d0085da72790539bd3515d6888917cc
хм да ето кисулькен <night.jesus.of.suburbia@yandex.ru>
2020-09-21 04:08:18 +0300

    lesson 2

---
+++ practice_4/src/ex_4/Square.java
@@ -0,0 +1,37 @@
+package ex_4;
+
+public class Square extends Rectangle{
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide(){
+        return this.length;
+    }
+
+    public void setSide(double side){
+        this.length = this.width = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        setSide(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square";
+    }
+}
